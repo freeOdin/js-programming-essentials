4a9d7c6de174351e5f38f074bb2853bd9263a463
freeOdin <159131665+freeOdin@users.noreply.github.com>
2024-10-09 22:01:10 +0100

    Add todo_list.html and todo_list.js

---
+++ ToDoList/todo_list.js
@@ -0,0 +1,47 @@
+const taskInput = document.querySelector("#taskInput");
+const taskList = document.querySelector("#taskList");
+
+const addTaskBtn = document.querySelector("#addTaskBtn");
+const clearCompletedBtn = document.querySelector("#clearCompletedBtn");
+const clearAllTasksBtn = document.querySelector("#clearAllTasksBtn");
+
+let tasks = [];
+
+addTaskBtn.addEventListener("click", addTask);
+clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllTasksBtn.addEventListener("click", clearAllTasks);
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    tasks.push({text: taskText});
+    taskInput.value = "";
+    displayTasks();
+  }
+}
+
+function displayTasks() {
+  taskList.innerHTML = "";
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+      <label for="task-${index}">${task.text}<label/>`;
+    li.querySelector("input").addEventListener("change", () => toggleTask(index));
+    taskList.appendChild(li);
+  }) 
+}
+
+function toggleTask(index) {
+  tasks[index].completed = !tasks[index].completed;
+  displayTasks();
+}
+
+function clearCompletedTasks() {
+  tasks = tasks.filter(task => !task.completed);
+  displayTasks();
+}
+
+function clearAllTasks() {
+  tasks = [];
+  displayTasks();
+}
